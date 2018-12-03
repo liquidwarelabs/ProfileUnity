af0003db840e7f020ca415d867ba38e5691a3131
Jack Smith <jack.smith@liquidwarelabs.com>
2018-12-03 15:50:30 -0600

    AppxProcess

--- Scripts/Modern App Selection/ProcessAppX.ps1
+++ Scripts/Modern App Selection/ProcessAppX.ps1
@@ -1,6 +1,8 @@
+# Set log time
+$datetime=get-date -format "mmddyyyyHHMMss"
 #Add apps you want to process in the first line
-$appxs="Microsoft.MicrosoftEdge,Microsoft.Windows.ShellExperienceHost,Microsoft.Windows.Cortana,windows.immersivecontrolpanel"
+$appxs="Microsoft.Windows.Cortana,Microsoft.Windows.ShellExperienceHost,windows.immersivecontrolpanel,Microsoft.WindowsCalculator,Microsoft.Windows.Photos"
 $appxs=$appxs.Split(",")
 foreach ($appx in $appxs) {
-Get-AppXPackage -AllUsers -name *$appx* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppXPackage -AllUsers -name *$appx* | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"; $_ } | Out-File "$env:temp\$datetime-LW-AppXPackage.log" -append
 }
