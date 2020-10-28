ff48ab9a52a041f2676f7767694cb56a93b7d657
Jack Smith <jack.smith@liquidwarelabs.com>
2020-10-28 16:01:06 -0500

    ProfileUnity

--- Scripts/Share Creation/ShareCreationScript.bat
+++ Scripts/Share Creation/ShareCreationScript.bat
@@ -1,14 +1,14 @@
 mkdir c:\ProfileShare
-net share ProfileShare="C:\ProfileShare" /GRANT:"authenticated users",FULL /Grant:"Domain admins",Full /Grant:"authenticated users",Full
-Icacls c:\ProfileShare /inheritance:r /grant:r "Domain Users":(S,RD,AD) /grant:r "Domain Admins":(OI)(CI)(F) /grant:r "creator owner":(OI)(CI)(IO)(F) /grant:r "authenticated users":(S,RD,AD) /grant:r "system":(OI)(CI)(F)
+net share ProfileShare="C:\ProfileShare" /GRANT:"authenticated users",FULL /Grant:"Domain admins",Full /Grant:"authenticated users",Full /Grant:"%computername%\administrator",Full
+Icacls c:\ProfileShare /inheritance:r /grant:r "Domain Users":(S,RD,AD) /grant:r "Domain Admins":(OI)(CI)(F) /grant:r "creator owner":(OI)(CI)(IO)(F) /grant:r "authenticated users":(S,RD,AD) /grant:r "system":(OI)(CI)(F) /grant:r "%computername%\administrator":(OI)(CI)(F)
 
 mkdir c:\ProfileUnity
 mkdir c:\ProfileUnity\ClientTools
 mkdir c:\ProfileUnity\User
 mkdir c:\ProfileUnity\Machine
 mkdir c:\ProfileUnity\FlexApps
-net share profileUnity="C:\ProfileUnity" /GRANT:"authenticated users",read /Grant:"Domain admins",Full /Grant:"Domain Users",read
-Icacls c:\ProfileUnity /inheritance:r /grant:r "Domain Users":(OI)(CI)(RX) /grant:r "Domain Admins":(OI)(CI)(F) /grant:r "authenticated users":(OI)(CI)(RX) /grant:r "system":(OI)(CI)(F)
+net share profileUnity="C:\ProfileUnity" /GRANT:"authenticated users",read /Grant:"Domain admins",Full /Grant:"Domain Users",read /Grant:"%computername%\administrator",Full
+Icacls c:\ProfileUnity /inheritance:r /grant:r "Domain Users":(OI)(CI)(RX) /grant:r "Domain Admins":(OI)(CI)(F) /grant:r "authenticated users":(OI)(CI)(RX) /grant:r "system":(OI)(CI)(F) /grant:r "%computername%\administrator":(OI)(CI)(F)
 
 Echo off
 Echo ---GPO startup path--- >> %userprofile%\desktop\ProfileUnityPaths.txt
