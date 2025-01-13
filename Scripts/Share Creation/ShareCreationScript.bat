bcf5950ef28639a22981c9c87524ff9e2fae34a4
Jack Smith <jack.smith@liquidwarelabs.com>
2025-01-13 08:50:10 -0600

    Updates

--- Scripts/Share Creation/ShareCreationScript.bat
+++ Scripts/Share Creation/ShareCreationScript.bat
@@ -1,5 +1,5 @@
 mkdir c:\ProfileShare
-net share ProfileShare="C:\ProfileShare" /GRANT:"authenticated users",FULL /Grant:"Domain admins",Full /Grant:"authenticated users",Full
+net share ProfileShare="C:\ProfileShare" /GRANT:"authenticated users",FULL /Grant:"Domain admins",Full /Grant:"authenticated users",Full /Grant:"Domain Computers",Full
 
 Icacls c:\ProfileShare /inheritance:r /grant:r "Domain Users":(S,RD,AD) /grant:r "Domain Admins":(OI)(CI)(F) /grant:r "creator owner":(OI)(CI)(IO)(F) /grant:r "authenticated users":(S,RD,AD) /grant:r "system":(OI)(CI)(F) /grant:r "Domain Computers":(OI)(CI)(F)
 
@@ -8,7 +8,7 @@ mkdir c:\ProfileUnity\ClientTools
 mkdir c:\ProfileUnity\User
 mkdir c:\ProfileUnity\Machine
 mkdir c:\ProfileUnity\FlexApps
-net share profileUnity="C:\ProfileUnity" /GRANT:"authenticated users",read /Grant:"Domain admins",Full /Grant:"Domain Users",read
+net share profileUnity="C:\ProfileUnity" /GRANT:"authenticated users",read /Grant:"Domain admins",Full /Grant:"Domain Users",read /Grant:"Domain Computers",Read
 
 Icacls c:\ProfileUnity /inheritance:r /grant:r "Domain Users":(OI)(CI)(RX) /grant:r "Domain Admins":(OI)(CI)(F) /grant:r "authenticated users":(OI)(CI)(RX) /grant:r "system":(OI)(CI)(F) /grant:r "Domain Computers":(OI)(CI)(F)
 
